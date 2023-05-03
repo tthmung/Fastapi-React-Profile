a1cde39a1f0df49acfb02e2d213c9f6573f886b3
Thawng Hmung <tthmung@mtu.edu>
2023-05-03 08:50:41 -0400

    Set up REST APIs

--- backend/main.py
+++ backend/main.py
@@ -21,8 +21,38 @@ async def root():
     return {"message": "Hello World"}
 
 
+@app.get("/experience")
+async def experience():
+    return {""}
+
+@app.get("/project")
+async def project():
+    return {""}
+
+
+@app.post("/login")
+async def login():
+    return
+
+@app.post("/new/experience")
+async def createExperience():
+    return
+
+@app.post("/new/project")
+async def createProject():
+    return
+
+@app.put("/update/experience")
+async def updateExperience():
+    return
+
+@app.put("/update/project")
+async def updateProject():
+    return
+
 
 if __name__ == "__main__":
     import uvicorn
 
+    # For development only
     uvicorn.run(app, host="0.0.0.0", port=8000, log_level="debug")
