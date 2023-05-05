a3c30aeb130b42ce35615acfd6d385201acf6769
Thawng Hmung <tthmung@mtu.edu>
2023-05-05 11:02:15 -0400

    Start working on homepage

--- frontend/src/Home/Home.tsx
+++ frontend/src/Home/Home.tsx
@@ -1,22 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import API from '../api'
-import api_helper from '../api_helper';
-import { Box } from '@chakra-ui/react';
+// import api_helper from '../api_helper';
+import { Box, Flex, Center, Image } from '@chakra-ui/react';
+
 import Header from './Header';
+import Card from './Card';
 
 
 export default function Home() {
 
     const [back, setBack] = useState({});
-    const api_help = new api_helper()
 
     useEffect(() => {
         const api = new API();
 
         const fetchData = async () => {
             let response = await api.postExperience();
-            var date = new Date(response.data.startDate);
-            console.log(api_help.getDate(date))
             setBack(response.data);
         }
 
@@ -27,6 +26,18 @@ export default function Home() {
     return (
         <>
             <Header />
+            <Box h="100vh">
+                <Flex justifyContent={"center"} alignItems={"center"}>
+                    <Center>
+                        Hello
+                    </Center>
+                    <Center>
+                        <Box>
+                            <Image src={require("../static/profile.JPG")} alt='Name' />
+                        </Box>
+                    </Center>
+                </Flex>
+            </Box>
         </>
     );
 }
